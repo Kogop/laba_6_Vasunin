1902e5e3add33ad6c821af1ce6cbd333c20f1961
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-12-16 12:43:05 +0300

    Добавьте файлы проекта.

---
+++ laba_6_Vasunin/Source.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+
+void chislo() {
+
+	FILE* chislo1 = fopen("1 chislo.txt", "wr");
+	char Chislo1[20];
+	char Chislo2[20];
+	for (int i = 0; i < 20; i++)
+	{
+		cin >> Chislo1[i];
+		fprintf(chislo1, "%10.6f", Chislo1[i]);
+
+	}
+
+	FILE* chislo2 = fopen("2 chislo.txt", "wr");
+
+	for (int i = 0; i < 20; i++)
+	{
+		cin >> Chislo2[i];
+		fprintf(chislo2, "%10.6f", Chislo2[i]);
+
+	}
+
+}
+
+
+
+
+
+
+FILE F;
+int main() {
+
+	chislo();
+
+
+	FILE* F = fopen("3 chislo.txt", "wr");
+
+
+
+
+	fclose(F);
+	return 1;
+}
